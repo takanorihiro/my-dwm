61a8ccc448ea69648ae0326e41e0ed82a0ef3743
nori <takanorihiro@protonmail.com>
2020-10-09 14:25:48 +0200

    change icon for tags

--- config.h
+++ config.h
@@ -38,17 +38,21 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, altbarclass
+	 *	WM_CLASS(STRING) = instance, altbarclass, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Chromium",  NULL,       NULL,       1 << 1,       0,           -1 },
-    { "Dunst",	   NULL,		NULL,		0,			1, 			-1	},
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Qutebrowser", NULL,    NULL,		  1 << 1,		0			 -1 },
+	{ "Spotify",	NULL,	  NULL,		  1 << 3,		1,			 -1 },
+	{ "Steam",		NULL,	  NULL,		  1 << 4,		1,			 -1 },
+	{ "Telegram",  NULL,	 NULL,		  1 << 2,		0,			 -1 },
+	{ "Dunst",	   NULL,	 NULL,			0,			1, 			 -1	},
 };
 
 /* layout(s) */
